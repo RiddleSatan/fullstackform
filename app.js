5510803475a2bfe306e4675ee53cb721e3bab120
Om Pandey <mynameisompandey007@gmail.com>
2024-05-07 07:16:56 +0530

    added multer

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import path from "path";
 import userModel from "./models/user.js";
-
+import multer from "multer";
+import crypto from 'crypto'
 
 const app = express();
 
@@ -11,6 +12,24 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+// for multer we need to setup the storage, multer is used as a middleware
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/files");//destination or the path of the file where the uploaded file is going to be stored
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(12, (err, bytes) => {
+      const fn = bytes.toString("hex") + path.extname(file.originalname);// generate a random name for everysingle file so that if any file with same name is being uploaded then it wont be replace the previous file but will have a new file name as newfile
+      //path.extname():this function is use to take out the extension of the file(Using file.orignalname) and then added it to the new file name
+      cb(null, fn);
+    });
+  },
+});
+  
+  
+const upload = multer({ storage });//it needs to be kept just after storage before any route, iy will be used as a middleware
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -52,18 +71,18 @@ app.post("/edit/:id", async (req, res) => {
     { name, email, imageurl },
     { new: true }
   );
-   res.redirect('/readuser')
+  res.redirect("/readuser");
   console.log(user);
 });
 
-app.get('/upload',(req,res)=>{
-  res.render('testMulter')
-})
+app.get("/upload", (req, res) => {
+  res.render("testMulter");
+});
 
-
-app.post('/upload',(req,res)=>{
+app.post("/upload", upload.single('file'),(req, res) => {
   // console.log(req.body) //req.body contains the values of the text fields of the form
- // console.log(req.file)//req.file contains the files uploaded via the form.
-})
+  // console.log(req.file)//req.file contains the files uploaded via the form.
+  console.log(req.file)
+});
 
 app.listen(3000);
